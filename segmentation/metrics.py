bc4bf9fe11c45b4b869f9b8ad83772ac1e35c733
Jon Deaton <jonpauldeaton@gmail.com>
2018-06-03 15:43:33 -0700

    change ddice semantics

--- segmentation/metrics.py
+++ segmentation/metrics.py
@@ -1,11 +1,25 @@
 
 import tensorflow as tf
 
-def dice_coeff(seg_true, seg_pred):
+def _get_class(seg, pred_class):
+    size = [-1] * 4
+    begin = [0] * 4
+
+    size[pred_class] = 1
+    begin[1] = pred_class
+
+    _seg = tf.slice(seg, begin=begin, size=size)
+    return _seg
+
+
+
+def dice_coeff(seg_true, seg_pred, pred_class=1):
     with tf.variable_scope("dice_coff_loss"):
         tf.assert_equal(tf.shape(seg_true), tf.shape(seg_pred))
-        seg_true_flat = tf.layers.flatten(seg_true)
-        seg_pred_flat = tf.layers.flatten(seg_pred)
+        
+        seg_true_flat = tf.layers.flatten(_get_class(seg_true, pred_class))
+        seg_pred_flat = tf.layers.flatten(_get_class(seg_pred, pred_class))
+
         intersection = tf.multiply(seg_true_flat, seg_pred_flat, name="intersection")
         intersect = tf.reduce_sum(intersection)
 
